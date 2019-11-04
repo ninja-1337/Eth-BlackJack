3010629f7f36cab321fb4af12415435198747cc9
Elias Evangelou <evangelou.e.95@gmail.com>
2019-11-04 19:44:36 +0000

    Adding a Cards game Library

--- Eth-BlackJack/Model.js
+++ Eth-BlackJack/Model.js
@@ -1,7 +1,6 @@
 import * as cards from "./cards";
 
 class Model {
-    stack = [];
     deck = new cards.Deck();
     constructor() {
         cards.init({table:'#card-table'});
@@ -9,12 +8,15 @@ class Model {
 
     }
     populateCardStack(){
-
         deck.addCards(cards.all);
         deck.render({immediate:true});
     }
 
     getCard() {
+
+    }
+
+    shuffleCards() {
 
     }
 
