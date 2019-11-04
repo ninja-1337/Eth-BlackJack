149dee5df66cf6fe22c8ad8b6ef3e54fe3ce0dc9
Elias Evangelou <evangelou.e.95@gmail.com>
2019-11-04 17:56:48 +0000

    Deleting unwanted stuff

--- Eth-BlackJack/View.js
+++ Eth-BlackJack/View.js
@@ -1,45 +1,11 @@
 class View {
+
+
     constructor() {
-        // The root element
-        this.app = this.getElement('#root')
 
-        // The title of the app
-        this.title = this.createElement('h1')
-        this.title.textContent = 'Todos'
-
-        // The form, with a [type="text"] input, and a submit button
-        this.form = this.createElement('form')
-
-        this.input = this.createElement('input')
-        this.input.type = 'text'
-        this.input.placeholder = 'Add todo'
-        this.input.name = 'todo'
-
-        this.submitButton = this.createElement('button')
-        this.submitButton.textContent = 'Submit'
-
-        // The visual representation of the todo list
-        this.todoList = this.createElement('ul', 'todo-list')
-
-        // Append the input and submit button to the form
-        this.form.append(this.input, this.submitButton)
-
-        // Append the title, form, and todo list to the app
-        this.app.append(this.title, this.form, this.todoList)
-    }
-    // ...
-
-    get _todoText() {
-        return this.input.value
     }
 
-    _resetInput() {
-        this.input.value = ''
-    }
-
-
-
-    displayTodos(todos) {
+    refreshGameScene() {
         // ...
     }
 
